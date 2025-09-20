ffe3cc12d60fddd614618fb9a4afe7b984725478
Rakeeb Hossain <hossainrakeeb7233@gmail.com>
2025-09-20 01:37:05 -0400

    Hero/Landing Page completed with header and footers

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,11 +2,22 @@ export default function Layout(props) {
     const { children } = props
 
     const header = (
-        <header></header>
+        <header>
+            <div>
+                <h1 className="text-gradient">COFFEETRACK</h1>
+                <p>For Coffee Insatiates</p>
+            </div>
+            <button>
+                <p>Sign up free</p>
+                <i className="fa-solid fa-mug-hot"></i>
+            </button>
+        </header>
     );
 
     const footer = (
-        <footer></footer>
+        <footer>
+            <p><span className="text-gradient">CoffeeTrack</span> was made by <a href="https://github.com/rakeebh7233/coffee-track" target="_blank">rakeebh7233</a>  using the <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library.</p>
+        </footer>
     );
 
     return (
